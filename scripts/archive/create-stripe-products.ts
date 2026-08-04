8c1299b7c2723730e24cdf9be7a402fe37375572
evolved-eden <desire1319@yahoo.com>
2026-08-03 20:47:34 -0400

    patch

--- scripts/archive/create-stripe-products.ts
+++ scripts/archive/create-stripe-products.ts
@@ -10,7 +10,7 @@
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-07-29.dahlia',
+  apiVersion: '2026-05-27.dahlia',
 })
 
 async function run() {
